6c4b51cf982c34b71338bd5ae1939e7435c82361
Jason Strimpel <jstrimpel@gmail.com>
2011-12-13 13:56:47 -0800

    set up part 2 dir

---
+++ examples/todos-requirejs-2/main.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';    
+    console.log('go go gadget main!');
+    
+    var config = {}, underscore, bootstrap;
+        // config.baseUrl = '../../test/vendor';
+        config.paths = {
+            'vendor' : '../../test/vendor',
+            'root' : '../..',
+            'examples' : '..'
+        };
+    
+    underscore = require({ context : 'underscore' });
+    bootstrap = require({ context : 'bootstrap', paths : config.paths });
+    underscore(['underscore'], function (_) {
+        window._ = _; // move underscore to globabl namespace for backbone 
+        bootstrap(['vendor/json2', 'order!vendor/jquery-1.6.4', 'order!root/backbone', 'order!examples/backbone-localstorage', 'order!todos.js'], function (a, b, c, d, app) {
+            app.init();                
+        });
+    });    
+}());
