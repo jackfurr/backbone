d00e07cef1bcd4260aa04e5a3448f6c2c47e90a8
Jason Strimpel <jstrimpel@gmail.com>
2012-03-23 17:26:03 -0700

    created directory for part 4

---
+++ examples/todos-requirejs-4/app.js
@@ -0,0 +1,15 @@
+define (['collections/todos', 'views/app', 'models/todo'], function (todosColl, appView, todosModel) {
+    'use strict';
+    
+    var module = {};
+    
+    module.init = function () {       
+        var collection = todosColl.create(), 
+            model = todosModel.create({ 'collection' : collection });  
+                  
+        collection.model = model;
+        return appView.create({ 'collection' : collection });             
+    };
+      
+    return module; 
+});
